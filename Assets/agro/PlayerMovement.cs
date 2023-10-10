9f636b41888a099abccd4464ac34e0b86a3ed56b
ArgusRow52 <elsi43916@gmail.com>
2023-10-10 11:02:58 +0300

    Update

--- Assets/agro/PlayerMovement.cs
+++ Assets/agro/PlayerMovement.cs
@@ -14,30 +14,32 @@ public class PlayerMovement : MonoBehaviour
     [SerializeField] private float _brakeTorque = 20000f;
 
     private bool isMoving; // Flag to track if the player is moving
+    private bool isRotatingLeft; // Flag to track left rotation
+    private bool isRotatingRight; // Flag to track right rotation
 
     private void FixedUpdate()
     {
         // Detect Oculus Quest controller input
-        float triggerValue = 0f;
-        InputDevice device = InputDevices.GetDeviceAtXRNode(XRNode.LeftHand);
-        device.TryGetFeatureValue(CommonUsages.trigger, out triggerValue);
+        float leftTriggerValue = 0f;
+        float rightTriggerValue = 0f;
 
-        if (triggerValue > 0.1f)
-        {
-            isMoving = true;
-        }
-        else
-        {
-            isMoving = false;
-        }
+        InputDevice leftDevice = InputDevices.GetDeviceAtXRNode(XRNode.LeftHand);
+        InputDevice rightDevice = InputDevices.GetDeviceAtXRNode(XRNode.RightHand);
+
+        leftDevice.TryGetFeatureValue(CommonUsages.trigger, out leftTriggerValue);
+        rightDevice.TryGetFeatureValue(CommonUsages.trigger, out rightTriggerValue);
+
+        isMoving = leftTriggerValue > 0.1f && rightTriggerValue > 0.1f;
+        isRotatingLeft = leftTriggerValue > 0.1f && rightTriggerValue < 0.1f;
+        isRotatingRight = rightTriggerValue > 0.1f && leftTriggerValue < 0.1f;
 
         Move();
 
-        if (Input.GetKey(KeyCode.A))
+        if (isRotatingLeft)
         {
             RotateLeft();
         }
-        else if (Input.GetKey(KeyCode.D))
+        else if (isRotatingRight)
         {
             RotateRight();
         }
@@ -46,12 +48,11 @@ public class PlayerMovement : MonoBehaviour
     private void Move()
     {
         float verticalInput = isMoving ? 1f : 0f;
-        float horizontalInput = Input.GetAxis("Horizontal");
 
         float flSpeed = verticalInput * _motorTorque;
         float frSpeed = verticalInput * _motorTorque;
-        float rlSpeed = horizontalInput * _motorTorque;
-        float rrSpeed = -horizontalInput * _motorTorque;
+        float rlSpeed = verticalInput * _motorTorque;
+        float rrSpeed = verticalInput * _motorTorque;
 
         _wheel1.motorTorque = flSpeed;
         _wheel2.motorTorque = frSpeed;
@@ -79,16 +80,6 @@ public class PlayerMovement : MonoBehaviour
         _wheel4.brakeTorque = torque;
     }
 
-    private void RotateLeft()
-    {
-        _carTransform.Rotate(Vector3.up, -_maxAngle * Time.deltaTime);
-    }
-
-    private void RotateRight()
-    {
-        _carTransform.Rotate(Vector3.up, _maxAngle * Time.deltaTime);
-    }
-
     private void ApplySteering(float steeringInput)
     {
         float maxSpeed = Mathf.Max(_wheel1.rpm, _wheel2.rpm, _wheel3.rpm, _wheel4.rpm);
@@ -103,5 +94,15 @@ public class PlayerMovement : MonoBehaviour
         _wheel2.steerAngle = frSteeringSpeed;
         _wheel3.steerAngle = rlSteeringSpeed;
         _wheel4.steerAngle = rrSteeringSpeed;
+    }
+
+    private void RotateLeft()
+    {
+        _carTransform.Rotate(Vector3.up, -_maxAngle * Time.deltaTime);
+    }
+
+    private void RotateRight()
+    {
+        _carTransform.Rotate(Vector3.up, _maxAngle * Time.deltaTime);
     }
 }
