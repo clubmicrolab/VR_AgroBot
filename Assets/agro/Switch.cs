54dbd33f84c12041c690edc9fd7915f9ec6df9fc
ArgusRow52 <elsi43916@gmail.com>
2023-10-12 16:12:18 +0300

    work

--- Assets/agro/Switch.cs
+++ Assets/agro/Switch.cs
@@ -5,6 +5,7 @@ public class Switch : MonoBehaviour
 {
     public PlayerMovement1 PlayerMovement1; // Initial active script
     public PlayerMovement2 PlayerMovement2; // Initial inactive script
+    public XRNode rightControllerNode = XRNode.RightHand; // Adjust as needed
 
     private bool isPlayerMovement1Active = true; // Flag to track active script
 
@@ -17,8 +18,8 @@ public class Switch : MonoBehaviour
 
     void Update()
     {
-        // Check if the "Back" button (Button B) is pressed on the Oculus Quest 2 controller
-        if (Input.GetButtonDown("B_Button"))
+        // Check if the "Switch" button (Button B) is pressed on the Oculus Quest 2 controller
+        if (IsSwitchButtonPressed())
         {
             // Toggle the active state of the scripts
             isPlayerMovement1Active = !isPlayerMovement1Active;
@@ -26,5 +27,13 @@ public class Switch : MonoBehaviour
             PlayerMovement1.enabled = isPlayerMovement1Active;
             PlayerMovement2.enabled = !isPlayerMovement1Active;
         }
+    }
+
+    private bool IsSwitchButtonPressed()
+    {
+        InputDevice device = InputDevices.GetDeviceAtXRNode(rightControllerNode);
+        bool isSwitchButtonPressed = false;
+        device.TryGetFeatureValue(CommonUsages.secondaryButton, out isSwitchButtonPressed);
+        return isSwitchButtonPressed;
     }
 }
